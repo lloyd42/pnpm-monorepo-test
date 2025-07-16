acc52b8e7155e40eb8f2ade26b6da25878c0c34e
lloyd42 <tioplato42@gmail.com>
2025-07-16 16:18:40 +0800

    chore: add biome

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,30 +1,28 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+import { Command } from "commander";
 import chalk from "chalk";
-import { add, minus } from '@tabemono/core';
+import { add, minus } from "@tabemono/core";
 
 const program = new Command();
 
+program.name("num cli").description("计算数字加减").version("0.0.1");
+
 program
-    .name('num cli')
-    .description('计算数字加减')
-    .version('0.0.1');
+	.command("add")
+	.description("加法")
+	.argument("a", "第一个数字")
+	.argument("b", "第二个数字")
+	.action((a: string, b: string) => {
+		console.log(chalk.green(add(+a, +b)));
+	});
 
-program.command('add')
-    .description('加法')
-    .argument('a', '第一个数字')
-    .argument('b', '第二个数字')
-    .action((a: string, b: string) => {
-        console.log(chalk.green(add(+a, +b)));
-    });
-
-
-program.command('minus')
-    .description('减法')
-    .argument('a', '第一个数字')
-    .argument('b', '第二个数字')
-    .action((a: string, b: string) => {
-        console.log(chalk.cyan(minus(+a, +b)));
-    });
+program
+	.command("minus")
+	.description("减法")
+	.argument("a", "第一个数字")
+	.argument("b", "第二个数字")
+	.action((a: string, b: string) => {
+		console.log(chalk.cyan(minus(+a, +b)));
+	});
 
 program.parse();
